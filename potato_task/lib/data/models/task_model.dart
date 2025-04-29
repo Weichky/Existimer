4ce2118f64eaf0c765314c401db97f08c96cf206
Weichky <WeichkyCharl@gmail.com>
2025-04-29 11:20:14 +0800

    创建了task.dart和task_model.dart

---
+++ potato_task/lib/data/models/task_model.dart
@@ -0,0 +1,11 @@
+class TaskModel {
+  final String id;
+  final String taskName;
+  final String timerUuid;
+
+  TaskModel({
+    required this.id,
+    required this.taskName,
+    required this.timerUuid,
+  });
+}
---
+++ potato_task/lib/domain/task/task.dart
@@ -0,0 +1,5 @@
+import 'package:potato_task/domain/timer/timer_unit.dart';
+
+class Task {
+  
+}
