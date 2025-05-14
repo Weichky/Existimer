3dd2f15d6892aa2a5491bcd41e1211de12d7e373
Weichky <WeichkyCharl@gmail.com>
2025-05-15 00:51:32 +0800

    编写了app_database.dart

--- potato_task/lib/data/repositories/app_database.dart
+++ potato_task/lib/data/repositories/app_database.dart
@@ -31,12 +31,53 @@ class AppDatabase {
       );
     ''');
 
-    //uuid与taskName映射表
+    //将meta和数据分离
     await db.execute('''
-      CREATE TABLE IF NOT EXISTS task_names (
+      CREATE TABLE IF NOT EXISTS task_meta (
         uuid TEXT PRIMARY KEY,
-        name TEXT NOT NULL
+        name TEXT NOT NULL,
+        type TEXT NOT NULL,
+        created_at TEXT,
+        archived INTEGER DEFAULT 0,
+        note TEXT
+      );
+    ''');
+
+    await db.execute('''
+      CREATE TABLE IF NOT EXISTS history (
+        id TEXT PRIMARY KEY,
+        uuid TEXT NOT NULL,
+        start_time TEXT NOT NULL,
+        note TEXT
+      );
+    ''');
+
+    await db.execute('''
+      CREATE TABLE IF NOT EXISTS settings (
+        key TEXT PRIMARY KEY,
+        value TEXT NOT NULL
       );
     ''');
   }
+
+
+  Future<bool> checkInitialized() async {
+    try {
+      final result = await _db.query(
+        'settings',
+        where: 'key = ?',
+        whereArgs: ['initialized'],
+        limit: 1,
+      );
+      return result.isNotEmpty;
+    } catch (e) {
+      return false;
+    }
+  }
+
+  Future<void> setInitializedFlag() async {
+    await _db.insert('settings', {'key': 'initialized', 'value': 1});
+  }
+
+  Database get db => _db;
 }
