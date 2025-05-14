2a9347f245a43f18e09ab950528d5fa40752ccf7
Weichky <WeichkyCharl@gmail.com>
2025-05-15 01:11:16 +0800

    编写了部分app_startup_service.dart

--- potato_task/lib/application/services/app_startup_service.dart
+++ potato_task/lib/application/services/app_startup_service.dart
@@ -1,2 +1,29 @@
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
+
+import 'package:potato_task/data/repositories/timer_unit/timer_unit_repository.dart';
+import 'package:potato_task/data/repositories/app_database.dart';
+
+class AppStartupService {
+  final AppDatabase _database;
+  final TimerUnitRepository _timerRepository;
+
+  AppStartupService(this._database, this._timerRepository);
+
+  Future<void> initializedApp() async {
+    await _database.init();
+
+    final isInitialized = await _database.checkInitialized();
+    if (!isInitialized) {
+      await _database.setupSchema();
+      await _database.setInitializedFlag();
+    }
+
+    await _recoverUnfinishedTimers();
+    // await _cleanupIfNeeded();
+  }
+
+  Future<void> _recoverUnfinishedTimers() async {
+    // 未完成逻辑
+  }
+}
