b9c7b930e22e84cecf7af820f6fae4a19b5b17a3
weichky <weichkycharl@gmail.com>
2025-06-25 23:03:15 +0800

    编写了部分timer_provider.dart，需要为timer_unit.dart添加fromSnapshot的工厂方法

--- lib/application/controllers/timer_controller.dart
+++ lib/application/controllers/timer_controller.dart
@@ -1,25 +1,31 @@
 import 'dart:async';
 
+import 'package:existimer/application/providers/database_provider.dart';
+import 'package:existimer/application/providers/settings_provider.dart';
+import 'package:existimer/data/repositories/timer_unit/timer_unit_sqlite.dart';
 import 'package:existimer/domain/timer/timer_unit.dart';
+import 'package:existimer/snapshots/timer_unit_snapshot.dart';
 import 'package:riverpod/riverpod.dart';
 
 //timer_controller.dart
-class TimerController {
-  final Ref _ref;
-  TimerUnit _unit;
-  Timer? _ticker;
+class TimerController extends AsyncNotifier<TimerUnit> {
+  late final TimerUnitSqlite repo;
 
-  TimerController({required Ref ref, required TimerUnit unit})
-    : _ref = ref,
-      _unit = unit;
+  @override
+  FutureOr<TimerUnit> build() async {
+    final db = await ref.watch(databaseProvider.future);
+    repo = TimerUnitSqlite(db.db);
 
-  TimerUnit get unit => _unit;
-
-  void start() {
-
+    final settings = await ref.watch(settingsProvider.future);
+    return settings.defaultTimerUnitType!.isCountup ?
+      TimerUnit.countup() :
+      TimerUnit.countdown(settings.countdownDuration!);
   }
 
-  void _startTicker() {
-    _ticker?.cancel();
+  Future<void> loadFromUuid(String uuid) async {
+    final TimerUnitSnapshot? snapshot = await repo.loadSnapshot(uuid);
+    if (snapshot != null) {
+      state = AsyncData(TimerUnit.fromSnapshot)
+    }
   }
 }
